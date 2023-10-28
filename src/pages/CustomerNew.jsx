9fcf214556d9c18a9fe2e8325dd85c7242d360dc
kiilove <jbkim@jncore.com>
2023-10-28 15:55:27 +0900

    commit on "main"

--- src/pages/CustomerNew.jsx
+++ src/pages/CustomerNew.jsx
@@ -1,12 +1,46 @@
 import React from "react";
 import { TitleBasicType } from "../components/ComponentTitles";
+import { Button, Form, Input, Space } from "antd";
 
 const CustomerNew = () => {
+  const [form] = Form.useForm();
+  const layout = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  };
+  const tailLayout = {
+    wrapperCol: {
+      offset: 8,
+      span: 16,
+    },
+  };
+
+  const onFinish = (values) => {
+    console.log(values);
+  };
   return (
-    <div className="flex w-full h-full bg-white rounded-lg flex-col justify-start items-start">
-      <div className="flex w-full h-auto p-2 ">
+    <div className="flex w-full h-full bg-white rounded-lg flex-col justify-start items-start p-6">
+      <div className="flex w-full h-auto">
         {TitleBasicType({ titleText: "고객등록" })}
       </div>
+      <Form onFinish={onFinish} form={form} {...layout}>
+        <Form.Item label="이름" name="customerName">
+          <Input />
+        </Form.Item>
+        <Form.Item label="전화번호" name="customerTel">
+          <Space size={10}>
+            <Input />
+          </Space>
+        </Form.Item>
+
+        <Form.Item>
+          <Button htmlType="submit">Primary Button</Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,21 +1,84 @@
-import React from "react";
-import MainMenus from "../components/MainMenus";
-import { defaultBgColor } from "../constColors";
-import CustomerNew from "./CustomerNew";
+import React, { useState } from "react";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, Button, theme } from "antd";
+import { useNavigate } from "react-router-dom";
+const { Header, Sider, Content } = Layout;
 
 const Main = ({ children }) => {
+  const [collapsed, setCollapsed] = useState(false);
+  const navigate = useNavigate();
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
-    <div
-      className="flex w-full"
-      style={{ backgroundColor: defaultBgColor, minHeight: "100vh" }}
-    >
-      <div className="flex">
-        <MainMenus />
-      </div>
-      <div className="flex w-full h-full p-2 ">
-        <div className="flex w-full h-full ">{children}</div>
-      </div>
-    </div>
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="flex w-full p-4">
+          <div className="w-full h-12 bg-gray-500 rounded-lg" />
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          className="text-base font-semibold "
+          defaultSelectedKeys={["1"]}
+          items={[
+            {
+              key: "1",
+              icon: <UserOutlined />,
+              label: "고객등록",
+              onClick: () => {
+                navigate("/customernew");
+              },
+            },
+            {
+              key: "2",
+              icon: <VideoCameraOutlined />,
+              label: "nav 2",
+            },
+            {
+              key: "3",
+              icon: <UploadOutlined />,
+              label: "nav 3",
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: "16px",
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          theme="dark"
+          style={{
+            margin: "24px 16px",
+            minHeight: 280,
+          }}
+          className=" rounded-lg"
+        >
+          {children}
+        </Content>
+      </Layout>
+    </Layout>
   );
 };
 
